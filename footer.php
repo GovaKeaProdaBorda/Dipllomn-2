32bfce715e32742b57329c4e2aaf463c6f2b23d1
GovaKeaProdaBorda <127184042+GovaKeaProdaBorda@users.noreply.github.com>
2025-06-18 00:56:16 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,4 @@
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </main>
 <footer class="footer">
     <div class="container">
@@ -5,37 +6,35 @@
             <div class="col-md-3">
                 <h5>Основные разделы</h5>
                 <ul>
-                    <li><a href="#">Цветы</a></li>
-                    <li><a href="#">Букеты</a></li>
-                    <li><a href="#">Композиции</a></li>
-                    <li><a href="#">Праздники</a></li>
-                    <li><a href="#">Всё для растений</a></li>
+                    <li><a href="catalog.php#Цветы">Цветы</a></li>
+                    <li><a href="catalog.php#Букеты">Букеты</a></li>
+                    <li><a href="catalog.php#Композиции">Композиции</a></li>
+                    <li><a href="catalog.php#Праздники">Праздники</a></li>
+                    <li><a href="catalog.php#Всё-для-растений">Всё для растений</a></li>
                 </ul>
             </div>
             <div class="col-md-3">
                 <h5>Информация</h5>
                 <ul>
-                    <li><a href="#">О нас</a></li>
-                    <li><a href="#">Оплата</a></li>
-                    <li><a href="#">Доставка</a></li>
-                    <li><a href="#">Гарантии</a></li>
-                    <li><a href="#">Контакты</a></li>
+                    <li><a href="about.php">О нас</a></li>
+                    <li><a href="payment.php">Оплата</a></li>
+                    <li><a href="delivery.php">Доставка</a></li>
+                    <li><a href="guarantees.php">Гарантии</a></li>
+                    <li><a href="contacts.php">Контакты</a></li>
                 </ul>
             </div>
             <div class="col-md-3">
                 <h5>Соц сети</h5>
                 <ul>
-                    <li><a href="#">Телеграмм</a></li>
-                    <li><a href="#">ВК</a></li>
-                    <li><a href="#">Одноклассники</a></li>
+                    <li><a href="https://web.telegram.org/">Телеграмм</a></li>
+                    <li><a href="http://vk.ru">ВК</a></li>
+                    <li><a href="https://ok.ru">Одноклассники</a></li>
                 </ul>
             </div>
             <div class="col-md-3">
                 <h5>Для пользователя</h5>
                 <ul>
-                    <li><a href="#">Личный кабинет</a></li>
-                    <li><a href="#">Политика конфиденциальности</a></li>
-                    <li><a href="#">Карта сайта</a></li>
+                    <li><a href="privacy.php">Политика конфиденциальности</a></li>
                 </ul>
             </div>
         </div>
